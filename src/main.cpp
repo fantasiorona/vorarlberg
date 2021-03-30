7f80f5abe3017fa4bd25cc04c466347eeed63113
Amon <Amon.Shokhin.Ahmed@gmail.com>
2021-03-30 15:10:56 +0200

    Implement crossover sensibly

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,24 @@
 #include <vector>
 
 void print_magic_square(std::vector<int> values);
+void print_genes(std::vector<int> values);
 void create_input_data();
 double evaluate_for_semi_magic_square(std::vector<int> genes);
 double evaluate_for_pandiagonal_magic_square(std::vector<int> genes);
 
 void mutate_genotype(std::vector<int> &genes, double mutationProbability);
+
+// creating vectors here so we dont have to do it in each crossover function
+std::vector<int> crossoverInverseSequence1;
+std::vector<int> crossoverInverseSequence2;
+std::vector<int> crossoverPositionSequence1;
+std::vector<int> crossoverPositionSequence2;
 void crossover_genotypes(std::vector<int> &, std::vector<int> &);
 int dimension = 0;
 // Create a new Population with genes consisting of three `double` variables
 // global because we use it the mutation function
 // yes this is spaghetti code
-Population<int> population("inputs/normal_dimension_4.txt", 100, 2500, 0.4, 0.8);
+Population<int> population("inputs/normal_dimension_3.txt", 100, 2500, 0.4, 0.8);
 int main() {
 
     // Uncomment to create input files
@@ -32,6 +39,11 @@ int main() {
                   << std::endl;
         return -1;
     }
+    // adjusting size according to variable count
+    crossoverInverseSequence1.resize(variableCount);
+    crossoverInverseSequence2.resize(variableCount);
+    crossoverPositionSequence1.resize(variableCount);
+    crossoverPositionSequence2.resize(variableCount);
 
     // Evolve the population to maximize the given equation
     population.evolve(evaluate_for_semi_magic_square, mutate_genotype, crossover_genotypes);
@@ -142,14 +154,97 @@ void mutate_genotype(std::vector<int> &genes, double mutationProbability) {
     }
 }
 
+// Adapted from https://github.com/guisehn/genetic-magic-square-finder
+// TODO: maybe put inverse and position vectors in a twodimensional vector so we reduce code
+// duplication
 void crossover_genotypes(std::vector<int> &genes1, std::vector<int> &genes2) {
-    // TODO: Implement sensibly
-    int cutoff_point = 3;
-    for (int i = 0; i < cutoff_point; i++) {
-        std::swap(genes1[i], genes2[i]);
+    // calculate inversion sequences
+    int geneAmount = genes1.size();
+    // inverse sequence counts the amount of bigger values to the left of the current value and
+    // stores it at the corresponding index
+    // so for example for the gene [2 3 4 5 1 6 7 8 9] we start with the value 1
+    // it is at index 5 and there are 4 bigger values to the left
+    // so the value in the inverse sequence at index 1 would be 4
+    // this uniquely identifies the gene but also allows to cross with out producing duplicates
+    // because translating back a inverse sequence only allows the creation of different values for
+    // each gene variable
+
+    // iterate over each value
+    for (int i = 0; i < geneAmount; ++i) {
+        // initialize to zero
+        crossoverInverseSequence1[i] = 0;
+        crossoverInverseSequence2[i] = 0;
+        bool found1 = false;
+        bool found2 = false;
+        // for each value we start at the left
+        for (int j = 0; j < geneAmount; ++j) {
+            // and continue until we find the value
+            if (!found1) {
+                // if we pass a bigger value we increment
+                if (genes1[j] > i + 1) {
+                    crossoverInverseSequence1[i] = crossoverInverseSequence1[i] + 1;
+                }
+                // stop when we find the value
+                if (genes1[j] == i + 1) {
+                    found1 = true;
+                }
+            }
+            // do same for the second parent
+            if (!found2) {
+                if (genes2[j] > i + 1) {
+                    crossoverInverseSequence2[i] = crossoverInverseSequence2[i] + 1;
+                }
+                if (genes2[j] == i + 1) {
+                    found2 = true;
+                }
+            }
+        }
+    }
+    // crossover afterwards is a simple swap operation with a randomly defined cutoff point
+    int cutoff_point = population.GetRandomGeneValue(0) - 1;
+    for (int i = cutoff_point; i < geneAmount; ++i) {
+        std::swap(crossoverInverseSequence1[i], crossoverInverseSequence2[i]);
+    }
+    // translate back to actual genes
+    // we start with creating a position vector
+    // this happens by counting greater or equal elements to the right of the current value
+    // this is basically the inverse operation to the inverse sequence creation and gives the
+    // information where each value is positioned in the gene vector
+    for (int i = geneAmount - 1; i >= 0; --i) {
+        // initialaze to current inverse value
+        crossoverPositionSequence1[i] = crossoverInverseSequence1[i];
+        crossoverPositionSequence2[i] = crossoverInverseSequence2[i];
+        // for each value to the right
+        for (int j = i + 1; j < geneAmount; ++j) {
+            // increase if its bigger or equal
+            if (crossoverPositionSequence1[j] >= crossoverPositionSequence1[i]) {
+                crossoverPositionSequence1[j] = crossoverPositionSequence1[j] + 1;
+            }
+            // do the same for the other parent
+            if (crossoverPositionSequence2[j] >= crossoverPositionSequence2[i]) {
+                crossoverPositionSequence2[j] = crossoverPositionSequence2[j] + 1;
+            }
+        }
+    }
+
+    // actually create genes by using the position information to find the position of the value
+    // add 1 because values are from 1-9 while vector indizes are from 0-8
+    for (int i = 0; i < geneAmount; ++i) {
+        genes1[crossoverPositionSequence1[i]] = i + 1;
+        genes2[crossoverPositionSequence2[i]] = i + 1;
     }
 }
 
+void print_genes(std::vector<int> values) {
+    for (int i = 0; i < values.size(); ++i) {
+        if (values[i] < 0) {
+            std::cout << "X, ";
+        } else {
+            std::cout << values[i] << ", ";
+        }
+    }
+    std::cout << std::endl;
+}
 void print_magic_square(std::vector<int> values) {
     std::cout << std::endl << "Magic Square:" << std::endl << std::endl;
     int magicConstant = (dimension * dimension + 1) * dimension / 2;
